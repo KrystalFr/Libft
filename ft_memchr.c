2896a3c0122068ec031eb1503b8b40c4ee5d32f6
Krystal FRANCOIS <krystal.francoi@hotmail.com>
2022-06-02 00:29:55 +0200

    really..???

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: kfrancoi <kfrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 22:13:29 by kfrancoi          #+#    #+#             */
-/*   Updated: 2022/05/09 14:58:36 by kfrancoi         ###   ########.fr       */
+/*   Updated: 2022/06/02 00:26:46 by kfrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t		i;
-	const char	*s2;
+	unsigned char	*s2;
 
 	i = 0;
-	s2 = s;
+	s2 = (unsigned char *)s;
 	while (i < n)
 	{
-		if (c == s2[i])
+		if ((unsigned char)c == s2[i])
 			return ((char *)s + i);
 		i++;
 	}
