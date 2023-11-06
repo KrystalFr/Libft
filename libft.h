6efdf0a5d7445057b32766edf4aeba234a0948f4
Krystal Francois <krfranco@bess-f2r1s20.clusters.42paris.fr>
2023-11-06 16:34:07 +0100

    the beginings

---
+++ libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krfranco <krfranco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 16:00:57 by krfranco          #+#    #+#             */
+/*   Updated: 2023/11/06 16:28:11 by krfranco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <stdio.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+#endif
